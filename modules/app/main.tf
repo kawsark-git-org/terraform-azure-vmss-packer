caccfe3ba35d92aca59435f47cb6f1fd0a3c105e
kawsark <kawsar@hashicorp.com>
2018-08-29 13:08:21 -0400

    test public ip

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -20,6 +20,13 @@ resource "azurerm_public_ip" "app-public-ip" {
   tags                         = "${var.tags}"
 }
 
+resource "azurerm_public_ip" "test" {
+  name                         = "PublicIPForLB"
+  location                     = "East US"
+  resource_group_name          = "${azurerm_resource_group.app.name}"
+  public_ip_address_allocation = "static"
+}
+
 resource "azurerm_lb" "vmss" {
   name                = "vmss-lb"
   location            = "${var.location}"
@@ -27,7 +34,7 @@ resource "azurerm_lb" "vmss" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = "${azurerm_public_ip.app-public-ip.id}"
+    public_ip_address_id = "${azurerm_public_ip.test.id}"
   }
 
   tags = "${var.tags}"
